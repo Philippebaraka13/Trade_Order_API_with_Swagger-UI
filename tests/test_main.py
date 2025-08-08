07cf062341b8c2765cb9c70ab82ed4faa440fcc8
Philippe Baraka <97373052+Philippebaraka13@users.noreply.github.com>
2025-08-08 13:49:37 -0400

    fixed from main import app, DATABASE, init_db

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,7 +2,8 @@
 import os
 import pytest
 from fastapi.testclient import TestClient
-from ..main import app, DATABASE, init_db
+from main import app, DATABASE, init_db
+
 # Triggering CI/CD
 client = TestClient(app)
 
