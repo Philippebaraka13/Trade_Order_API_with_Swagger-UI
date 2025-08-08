51b7633a14ec405864ad5b382a3ca2a146207cd8
Philippe Baraka <97373052+Philippebaraka13@users.noreply.github.com>
2025-08-08 13:40:34 -0400

    Trigger CI/CD

--- tests/test_main.py
+++ tests/test_main.py
@@ -3,7 +3,7 @@ import os
 import pytest
 from fastapi.testclient import TestClient
 from ..main import app, DATABASE, init_db
-
+# Triggering CI/CD
 client = TestClient(app)
 
 @pytest.fixture(scope="session", autouse=True)
